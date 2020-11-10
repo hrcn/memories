a4f5f28836a9656445a9362ea13cbb14ec5109c7
Rui Han <hrcn1128@gmail.com>
2020-11-09 23:23:57 -0500

    added store and reducer

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,10 @@
+export default (posts = [], action) => {
+  switch (action.type) {
+    case 'FETCH_ALL':
+      return psots;
+    case 'CREATE':
+      return psots;
+    default:
+      return psots;
+  }
+}
